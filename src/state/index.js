7912f0ad8e2b6bffde253bbb8eccd3a717e2c929
Gamal Samadov <gamalsamadov@gmail.com>
2024-01-14 14:01:54 +0300

    Navbar design finished 📌

--- src/state/index.js
+++ src/state/index.js
@@ -44,5 +44,5 @@ export const authSlice = createSlice({
 	}
 }) 
 
-export const { setModule, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions
 export default authSlice.reducer
